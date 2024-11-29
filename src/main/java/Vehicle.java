952c3fcc5fa7b055ded5fcdfe4ab2b62bb152039
zarapdev <java.zaradeveloper@gmail.com>
2024-11-29 21:19:43 +0400

    initial commit

---
+++ src/main/java/Vehicle.java
@@ -0,0 +1,38 @@
+public class Vehicle {
+    private String brand;
+    private String model;
+    private int year;
+
+    public Vehicle(String brand, String model, int year) {
+        this.brand = brand;
+        this.model = model;
+        this.year = year;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+    public void displayInfo(){
+        System.out.println("brand ;" + brand + " model " +  model + " year " + year );
+    }
+}
